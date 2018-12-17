6e3872ff14f192b86b5a7a09fd1b2138e2f0ca37
hadiiiiii <45215673+hadiiiiii@users.noreply.github.com>
2018-12-17 12:32:35 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
     // ID van de categorie van de tickets.
-    const categoryId = "498092615351271425";
+    const categoryId = "524319917005078539";
 
     // Verkrijg Gebruikersnaam
     var userName = message.author.username;
