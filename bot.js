d20c53c26d942cb9ee94d41fa66879011fc12c1b
hadiiiiii <45215673+hadiiiiii@users.noreply.github.com>
2018-12-17 12:32:16 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,88 +1,77 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const discord = require("discord.js");
 
-function clean(text) {
-    if (typeof(text) === "string")
-      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-    else
-        return text;
-}
+module.exports.run = async (bot, message, args) => {
 
-const prefix = "-";
-const categoryId = "524319917005078539";
+    // ID van de categorie van de tickets.
+    const categoryId = "498092615351271425";
 
-client.on("ready", () => {
-  console.log("Vulnix | Logged in! Server count: ${client.guilds.size}");
-});
+    // Verkrijg Gebruikersnaam
+    var userName = message.author.username;
+    // Verkrijg discriminator
+    var userDiscriminator = message.author.discriminator;
 
+    // Als ticket al gemaakt is
+    var bool = false;
 
-client.on("message", (message) => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+    // Kijk na als ticket al gemaakt is.
+    message.guild.channels.forEach((channel) => {
 
-  if (message.content.toLowerCase().startsWith(prefix + `help`)) {
-    const embed = new Discord.RichEmbed()
-    .setTitle(`:mailbox_with_mail: Vulnix Help`)
-    .setColor(0xCF40FA)
-    .setDescription(`Hello! I'm Vulnix, the Discord bot for super cool support ticket stuff and more! Here are my commands:`)
-    .addField(`Tickets`, `[${prefix}new]() > Opens up a new ticket and tags the Support Team\n[${prefix}close]() > Closes a ticket that has been resolved or been opened by accident`)
-    .addField(`Other`, `[${prefix}help]() > Shows you this help menu your reading\n[${prefix}ping]() > Pings the bot to see how long it takes to react\n[${prefix}about]() > Tells you all about Vulnix`)
-    message.channel.send({ embed: embed });
-  }
+        // Als ticket is gemaakt, zend bericht.
+        if (channel.name == userName.toLowerCase() + "-" + userDiscriminator) {
 
-  if (message.content.toLowerCase().startsWith(prefix + `ping`)) {
-    message.channel.send(`Hoold on!`).then(m => {
-    m.edit(`:ping_pong: Wew, made it over the ~waves~ ! **Pong!**\nMessage edit time is ` + (m.createdTimestamp - message.createdTimestamp) + `ms, Discord API heartbeat is ` + Math.round(client.ping) + `ms.`);
+            message.channel.send("Je hebt al een ticket aangemaakt");
+
+            bool = true;
+
+        }
+
     });
-}
 
-if (message.content.toLowerCase().startsWith(prefix + `new`)) {
-    const reason = message.content.split(" ").slice(1).join(" ");
-    if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
-    if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
-    message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
-        let role = message.guild.roles.find("name", "Support Team");
-        let role2 = message.guild.roles.find("name", "@everyone");
-        c.overwritePermissions(role, {
-            SEND_MESSAGES: true,
-            READ_MESSAGES: true
-        });
-        c.overwritePermissions(role2, {
-            SEND_MESSAGES: false,
-            READ_MESSAGES: false
-        });
-        c.overwritePermissions(message.author, {
-            SEND_MESSAGES: true,
-            READ_MESSAGES: true
-        });
-        message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
-        const embed = new Discord.RichEmbed()
-        .setColor(0xCF40FA)
-        .addField(`Hey ${message.author.username}!`, `Please try explain why you opened this ticket with as much detail as possible. Our **Support Team** will be here soon to help.`)
-        .setTimestamp();
-        c.send({ embed: embed });
-    }).catch(console.error);
-}
-if (message.content.toLowerCase().startsWith(prefix + `close`)) {
-    if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
+    // Als ticket return code.
+    if (bool == true) return;
 
-    message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`-confirm\`. This will time out in 10 seconds and be cancelled.`)
-    .then((m) => {
-      message.channel.awaitMessages(response => response.content === '-confirm', {
-        max: 1,
-        time: 10000,
-        errors: ['time'],
-      })
-      .then((collected) => {
-          message.channel.delete();
-        })
-        .catch(() => {
-          m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
-              m2.delete();
-          }, 3000);
+    var embedCreateTicket = new discord.RichEmbed()
+        .setTitle("Hoi, " + message.author.username)
+        .setFooter("Support kanaal wordt aangemaakt");
+
+    message.channel.send(embedCreateTicket);
+
+    // Maak kanaal en zet in juiste categorie.
+    message.guild.createChannel(userName + "-" + userDiscriminator, "text").then((createdChan) => { // Maak kanaal
+
+        createdChan.setParent(categoryId).then((settedParent) => { // Zet kanaal in category.
+
+            // Zet perms voor iedereen
+            settedParent.overwritePermissions(message.guild.roles.find('name', "@everyone"), { "READ_MESSAGES": false });
+            // Zet perms voor de gebruiker die ticket heeft aangemaakt.
+            settedParent.overwritePermissions(message.author, {
+
+                "READ_MESSAGES": true, "SEND_MESSAGES": true,
+                "ATTACH_FILES": true, "CONNECT": true,
+                "CREATE_INSTANT_INVITE": false, "ADD_REACTIONS": true
+
+            });
+
+            var embedParent = new discord.RichEmbed()
+                .setTitle("Hoi, " + message.author.username.toString())
+                .setDescription("Zet hier je vraag/bericht");
+
+            settedParent.send(embedParent);
+
+        }).catch(err => {
+            message.channel.send("Er is iets fout gelopen.");
         });
+
+    }).catch(err => {
+        message.channel.send("Er is iets fout gelopen.");
     });
+
 }
 
-});
+module.exports.help = {
+    name: "ticket"
+}
+
+
 
 client.login(process.env.BOT_TOKEN);
