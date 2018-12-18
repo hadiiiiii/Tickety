4cdc2e40c5efdb91d63cbfa59c1221bfc08f5539
hadiiiiii <45215673+hadiiiiii@users.noreply.github.com>
2018-12-18 03:07:19 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-function clean(text) {
-    if (typeof(text) === "string")
-      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-    else
-        return text;
-}
-
-const prefix = "-";
-
-client.on("ready", () => {
-  console.log("Vulnix | Logged in! Server count: ${client.guilds.size}");
-  client.user.setGame(`Support Magic |${prefix}new`);
-});
-
 
 client.on("message", (message) => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
@@ -38,7 +24,11 @@ client.on("message", (message) => {
 if (message.content.toLowerCase().startsWith(prefix + `new`)) {
     const reason = message.content.split(" ").slice(1).join(" ");
     if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
+
+    if (message.guild.category.exists("name", "Tickets" + message.author.id)) return message.channel.send(`!`);
+
     if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
+    message.createCategory("Tickets", "category")
     message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
         let role = message.guild.roles.find("name", "Support Team");
         let role2 = message.guild.roles.find("name", "@everyone");
@@ -84,6 +74,10 @@ if (message.content.toLowerCase().startsWith(prefix + `close`)) {
 }
 
 });
+
+
+
+
 
 
 client.login(process.env.BOT_TOKEN);
